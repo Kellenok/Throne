996624ab174ff118a3f8756edfa4c986df1804e2
Kellen <kputuhuk@gmail.com>
2026-04-26 13:02:32 +0300

    Fix: move AmneziaWG if statements inside the loop

--- src/database/SettingsRepo.cpp
+++ src/database/SettingsRepo.cpp
@@ -358,8 +358,7 @@ namespace Configs {
                 else if (key == "warp_public_key") warp_public_key = varValue.toString();
                 else if (key == "warp_ifc_addrs") warp_ifc_addrs = varValue.toStringList();
 else if (key == "warp_ep") warp_ep = varValue.toString();
-                else if (key == "enable_dns_routing") enable_dns_routing = varValue.toBool();
-            }
+            else if (key == "enable_dns_routing") enable_dns_routing = varValue.toBool();
 
             // AmneziaWG settings - separate if statements to avoid MSVC nesting limit
             if (key == "warp_amneziawg") warp_amneziawg = varValue.toBool();
