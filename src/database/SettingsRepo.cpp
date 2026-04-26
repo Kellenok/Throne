3bff028537939ab2284523b20372a340273422cb
Kellen <kputuhuk@gmail.com>
2026-04-26 12:31:52 +0300

    Fix: revert broken if-else

--- src/database/SettingsRepo.cpp
+++ src/database/SettingsRepo.cpp
@@ -365,38 +365,22 @@ namespace Configs {
                 else if (key == "warp_S1") warp_S1 = varValue.toInt();
                 else if (key == "warp_S2") warp_S2 = varValue.toInt();
                 else if (key == "warp_S3") warp_S3 = varValue.toInt();
-                } else if (key == "warp_S4") {
-                    warp_S4 = varValue.toInt();
-                } else if (key == "warp_H1_min") {
-                    warp_H1_min = varValue.toInt();
-                } else if (key == "warp_H1_max") {
-                    warp_H1_max = varValue.toInt();
-                } else if (key == "warp_H2_min") {
-                    warp_H2_min = varValue.toInt();
-                } else if (key == "warp_H2_max") {
-                    warp_H2_max = varValue.toInt();
-                } else if (key == "warp_H3_min") {
-                    warp_H3_min = varValue.toInt();
-                } else if (key == "warp_H3_max") {
-                    warp_H3_max = varValue.toInt();
-                } else if (key == "warp_H4_min") {
-                    warp_H4_min = varValue.toInt();
-                } else if (key == "warp_H4_max") {
-                    warp_H4_max = varValue.toInt();
-                } else if (key == "warp_I1") {
-                    warp_I1 = varValue.toString();
-                } else if (key == "warp_I2") {
-                    warp_I2 = varValue.toString();
-                } else if (key == "warp_I3") {
-                    warp_I3 = varValue.toString();
-                } else if (key == "warp_I4") {
-                    warp_I4 = varValue.toString();
-                } else if (key == "warp_I5") {
-                    warp_I5 = varValue.toString();
-                } else if (key == "enable_dns_routing") {
-                    enable_dns_routing = varValue.toBool();
-                } else if (key == "inbound_auth") {
-                else if (key == "inbound_user") inbound_user = varValue.toString();
+                else if (key == "warp_S4") warp_S4 = varValue.toInt();
+                else if (key == "warp_H1_min") warp_H1_min = varValue.toInt();
+                else if (key == "warp_H1_max") warp_H1_max = varValue.toInt();
+                else if (key == "warp_H2_min") warp_H2_min = varValue.toInt();
+                else if (key == "warp_H2_max") warp_H2_max = varValue.toInt();
+                else if (key == "warp_H3_min") warp_H3_min = varValue.toInt();
+                else if (key == "warp_H3_max") warp_H3_max = varValue.toInt();
+                else if (key == "warp_H4_min") warp_H4_min = varValue.toInt();
+                else if (key == "warp_H4_max") warp_H4_max = varValue.toInt();
+                else if (key == "warp_I1") warp_I1 = varValue.toString();
+                else if (key == "warp_I2") warp_I2 = varValue.toString();
+                else if (key == "warp_I3") warp_I3 = varValue.toString();
+                else if (key == "warp_I4") warp_I4 = varValue.toString();
+                else if (key == "warp_I5") warp_I5 = varValue.toString();
+                else if (key == "enable_dns_routing") enable_dns_routing = varValue.toBool();
+                else if (key == "inbound_auth") inbound_auth = varValue.toBool();
                 else if (key == "inbound_pass") inbound_pass = varValue.toString();
                 else if (key == "allow_stopping_active_profile") allow_stopping_active_profile = varValue.toBool();
                 else if (key == "disable_mixed_inbound") disable_mixed_inbound = varValue.toBool();
